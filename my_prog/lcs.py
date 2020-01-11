7f780423c8b5b51f88d5c51e605109857f6d3c7f
spartak0506 <spartak0506@gmail.com>
2020-01-12 00:07:37 +0800

    Нахождение наибольшей общей последовательности, файл lcs

---
+++ my_prog/lcs.py
@@ -0,0 +1,17 @@
+# Наибольшая общая подпоследовательность
+
+
+def lcs(A,B):
+    F = [[0]*(len(B)+1) for i in range(len(A)+1)]
+    for i in range(1, len(A)+1):
+        for j in range(1, len(B)+1):
+            if A[i-1] == B[j-1]:
+                F[i][j] = 1 + F[i-1][j-1]
+            else:
+                F[i][j] = max(F[i-1][j], F[i][j-1])
+    return F[-1][-1]
+
+A = range(70)
+B = range(80)
+
+print(lcs(A,B))
