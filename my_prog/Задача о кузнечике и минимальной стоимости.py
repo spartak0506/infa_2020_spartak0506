cae5911fd687f2244794bdce88debaa53ccd03b1
Darya <spartak0506@gmail.com>
2020-01-12 20:58:21 +0700

    Дополнил условием задачу о стоимости

--- my_prog/Задача о кузнечике и минимальной стоимости.py
+++ my_prog/Задача о кузнечике и минимальной стоимости.py
@@ -3,7 +3,18 @@
 
 def fib(n, price:list):
     A = [-1,0]+[0] * n
+    
+    path = [0] * n
+    
     for i in range(0, n+1):
         A[i] = min((A[i-1] + price[i]), (A[i-2] + price[i]))
+        
+        if A[i-1] < A[i-2]:
+            path.append(A[i-1])
+        else:
+            path.append(A[i-2])
+        print(path)
+    
     return A[n]
+    
     
