dd1d995bd38f56151fea503c6ccf0dc9d3055ff8
spartak0506 <spartak0506@gmail.com>
2020-01-12 12:51:22 +0800

    Добавил задачу про кузнечика и минимальную стоимость

---
+++ my_prog/Задача о кузнечике и минимальной стоимости.py
@@ -0,0 +1,9 @@
+# the first element does not count
+# the jump begins with [0]
+
+def fib(n, price:list):
+    A = [-1,0]+[0] * n
+    for i in range(0, n+1):
+        A[i] = min((A[i-1] + price[i]), (A[i-2] + price[i]))
+    return A[n]
+    
